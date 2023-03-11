7408eb9c7d964c13b237105b3306b2d4e76a74ed
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-11 23:52:48 +0300

    Typecasting

---
+++ TypeCasting.java
@@ -0,0 +1,39 @@
+//object type casting code in java?
+
+// Constructor Chaining to Another Class in Java
+
+class Account{
+  
+    Account(String first_name, int your_age){
+        String fname = first_name;
+        int age = your_age;
+  
+        System.out.println("\nThe name entered is " + fname);
+        System.out.println("Your are " + age + " years old.");
+    }
+  
+    Account(){
+        System.out.println("\nWelcome dear customer");
+    }
+  
+    public static void main(String args[]){
+        FixedDeposit acct = new FixedDeposit();
+    }
+  
+ }
+  
+ class FixedDeposit extends Account{
+  
+    FixedDeposit(){
+  
+        super(); // calling the no-arg constructor in the base class
+        double APY = 12.5;
+        System.out.println("Your current interest rate is " + APY + "%");
+    }
+ }
+
+
+
+   
+
+
