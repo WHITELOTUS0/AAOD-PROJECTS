54d71800d9a86db1af27b0b9fdeffcab38607edc
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-24 13:38:58 +0300

    cloning

---
+++ Student.java
@@ -0,0 +1,25 @@
+//Object cloning in java
+public class Student implements Cloneable {
+
+    String name;
+    int age;
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public static void main(String[] args) {
+        try{
+        Student obj = new Student("GLORRY", 20);
+        Student obj1 = (Student) obj.clone();
+        System.out.println(obj.name +""+obj.age);
+        System.out.println(obj1);
+        }
+        catch(CloneNotSupportedException e){}
+    }
+}
