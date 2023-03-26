e1ae0c7799e842ceb875020bac7b843fdc0fc8e9
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-26 19:56:37 +0300

    main method

--- Interning.java
+++ Interning.java
@@ -2,4 +2,8 @@ public class Interning{
     String s1 = new String("Glorry");
     String s2 = new String("Glorry");
     String s3 = s1.intern();
+    
+    public static void main(String[] args) {
+        
+    }
 }
