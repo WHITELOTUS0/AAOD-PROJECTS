e1fa00aa4a5acacfdf65669243f0f2091c14bec3
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-26 19:58:46 +0300

    cleaning and removing errors

--- Interning.java
+++ Interning.java
@@ -1,9 +1,10 @@
 public class Interning{
-    String s1 = new String("Glorry");
-    String s2 = new String("Glorry");
-    String s3 = s1.intern();
+   
     
     public static void main(String[] args) {
-        System.out.println();
+        String s1 = new String("Glorry");
+        String s2 = new String("Glorry");
+        String s3 = s1.intern();
+        System.out.println(s3);
     }
 }
