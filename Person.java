182ca8e9936c7d41fc084528f59191751c25c745
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-29 23:16:20 +0300

    main method

--- Person.java
+++ Person.java
@@ -21,6 +21,8 @@ public final class Person{
 
     public static void main(String[] args) {
         Person p = new Person("John", 30);
+        p.withAge(0);
+        System.out.println(p.withAge(1));
     }
 
 }
