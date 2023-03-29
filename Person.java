d4d135598ea27c80f77e2406518244273c79f82f
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-29 23:04:03 +0300

    initializing variable

--- Person.java
+++ Person.java
@@ -2,6 +2,8 @@ public final class Person{
     private final String name;
     private final int age;
 
-    Person(String name, int age){}
+    Person(String name, int age){
+        this.name = name;
+    }
 
 }
