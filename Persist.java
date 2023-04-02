24f75cedabcdbebf563c7365ece643f2766e7f59
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-02 23:14:28 +0300

    creating stream

--- Persist.java
+++ Persist.java
@@ -3,7 +3,8 @@ import java.io.*;
 public class Persist{
     public static void main(String[] args) {
         try{
-            Student2 s = new Student2(211, "Sharo")
+            Student2 s = new Student2(211, "Sharo");
+            FileOutputStream fout = new FileOutputStream("f.txt");
         }catch(Exception e){System.out.println(e);}
     }
 }
