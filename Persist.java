557be200a15507c1fbc3c6757801c6bd0751830f
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-02 23:15:10 +0300

    writing the object

--- Persist.java
+++ Persist.java
@@ -5,6 +5,7 @@ public class Persist{
         try{
             Student2 s = new Student2(211, "Sharo");
             FileOutputStream fout = new FileOutputStream("f.txt");
+            ObjectOutputStream oout = new ObjectOutputStream(fout);
         }catch(Exception e){System.out.println(e);}
     }
 }
