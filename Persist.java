6cf1036a52e3027da4c9341999485b8c5633991c
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-02 23:11:58 +0300

    try block

--- Persist.java
+++ Persist.java
@@ -2,6 +2,8 @@ import java.io.*;
 
 public class Persist{
     public static void main(String[] args) {
-        
+        try{
+            
+        }catch(Exception e){System.out.println(e);}
     }
 }
