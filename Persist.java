8e0f9a271bef9d0cf0a1f234cc02a042ac847176
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-02 23:16:47 +0300

    writin

--- Persist.java
+++ Persist.java
@@ -3,9 +3,10 @@ import java.io.*;
 public class Persist{
     public static void main(String[] args) {
         try{
-            Student2 s = new Student2(211, "Sharo");
+            Student2 s1 = new Student2(211, "Sharo");
             FileOutputStream fout = new FileOutputStream("f.txt");
             ObjectOutputStream oout = new ObjectOutputStream(fout);
+            oout.writeObject(s1);
         }catch(Exception e){System.out.println(e);}
     }
 }
