054a7669469df22d48a8d3b9f39c4781667ff94a
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-02 23:13:08 +0300

    creating object

--- Persist.java
+++ Persist.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Persist{
     public static void main(String[] args) {
         try{
-            
+            Student2 s = new Student2(211, "Sharo")
         }catch(Exception e){System.out.println(e);}
     }
 }
